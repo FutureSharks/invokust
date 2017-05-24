7d1cbd5037bd13f474a24d662b4ca8eb5bf87797
futuresharks <futuresharks@gmail.com>
2017-05-24 14:21:10 +0200

    Bump pypi version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name = 'invokust',
-    version = '0.4',
+    version = '0.41',
     author = 'Max Williams',
     author_email = 'futuresharks@gmail.com',
     url = 'https://github.com/FutureSharks/invokust',
-    download_url  =  'https://github.com/FutureSharks/invokust/archive/0.4.tar.gz',
+    download_url  =  'https://github.com/FutureSharks/invokust/archive/0.41.tar.gz',
     license = 'GPLv2',
     description = 'A small wrapper for locust to allow load testing on AWS Lambda',
     scripts = ['invokr.py'],
