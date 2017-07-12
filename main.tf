bc35888a615a103fe67a0ab1a7553c386358f02d
futuresharks <futuresharks@gmail.com>
2017-07-12 14:33:25 +0200

    Adding IAM policy to TF example

--- main.tf
+++ main.tf
@@ -1,11 +1,54 @@
 # Example terraform configuration for a lambda function that uses invokust to run locust
 
-data "aws_caller_identity" "current" {}
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_iam_role" "lambda_basic_execution" {
+  name               = "lambda_basic_execution"
+  path               = "/service-role/"
+  assume_role_policy = "${data.aws_iam_policy_document.lambda_basic_execution_assume_role.json}"
+}
+
+data "aws_iam_policy_document" "lambda_basic_execution_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "lambda_basic_execution" {
+  statement {
+    effect  = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "lambda_basic_execution" {
+  name        = "lambda_basic_execution"
+  description = "Basic execution policy for a Lambda function"
+  policy      =  "${data.aws_iam_policy_document.lambda_basic_execution.json}"
+}
+
+resource "aws_iam_policy_attachment" "lambda_basic_execution" {
+  name       = "lambda_basic_execution"
+  roles      = ["${aws_iam_role.lambda_basic_execution.name}"]
+  policy_arn = "${aws_iam_policy.lambda_basic_execution.arn}"
+}
 
 resource "aws_lambda_function" "lambda_locust" {
   filename      = "invokust_example.zip"
   function_name = "lambda_locust"
-  role          = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/lambda_basic_execution"
+  role          = "${aws_iam_role.lambda_basic_execution.arn}"
   handler       = "lambda_locust.handler"
   runtime       = "python3.6"
   timeout       = 300
