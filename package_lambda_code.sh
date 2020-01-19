8f6df73e3e49a10d0760e9798f482c0eca9098da
agolenkovskiy <agolenkovskiy@aligntech.com>
2020-01-19 15:42:24 -0500

    Update bash script to get rid of version

--- package_lambda_code.sh
+++ package_lambda_code.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+rm -rf python-packages
+
 python3 setup.py bdist_wheel
 
-docker run -it -v $PWD/python-packages:/python-packages -v $PWD/dist:/dist python:3.6 bash -c "pip install dist/invokust-0.6-py3-none-any.whl --target=/python-packages"
+docker run -it -v $PWD/python-packages:/python-packages -v $PWD/dist:/dist python:3.6 bash -c "pip install dist/invokust-*.whl --target=/python-packages"
 
 zip -q -r lambda_locust.zip lambda_locust.py locustfile_example.py python-packages
